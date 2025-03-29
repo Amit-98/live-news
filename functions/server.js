1c457a1e6df852c53298c245c8397666147ff2c9
Amit Yadav <59062171+Amit-98@users.noreply.github.com>
2025-03-29 22:05:24 +0530

    some changes in view

--- functions/server.js
+++ functions/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 //let file = require("../views/index.ejs");
 // View engine setup
