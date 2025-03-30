45ba481ee52e43a7e380321d0e2d62f376073983
Amit Yadav <59062171+Amit-98@users.noreply.github.com>
2025-03-30 18:14:03 +0530

    changes something

--- functions/server.js
+++ functions/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
+app.use((req, _, next) => {
   req.url = req.url.replace(/^\/\.netlify\/functions\/server/, "");
   next();
 });
